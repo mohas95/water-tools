361421e7b94b6a1384c06fb80282fe8794ec84ee
moha7108 <moha7108@pm.me>
2021-07-27 15:33:40 -0400

    add ph up and down methods

--- ph-control.py
+++ ph-control.py
@@ -2,10 +2,39 @@ import RPi.GPIO as GPIO
 import time
 import threading
 
-## Define Global variables
+## Define RPI Pins
 ph_up = 26 # Relay_Ch1 = 26
 ph_down = 20 # Relay_Ch2 = 20
 # Relay_Ch3 = 21
+
+
+## Define Global variables
+high_ph_thresh =8
+low_ph_thresh = 7
+
+
+def stop_all():
+	'''
+	'''
+	GPIO.output(ph_up, GPIO.HIGH)
+	GPIO.output(ph_down, GPIO.HIGH)
+
+
+def PH_up(on_time = 5):
+	'''
+	'''
+	GPIO.output(ph_up, GPIO.LOW)
+	time.sleep(on_time)
+	GPIO.output(ph_up, GPIO.HIGH)
+
+def PH_down(on_time = 5):
+	'''
+	'''
+	GPIO.output(ph_down, GPIO.LOW)
+	time.sleep(on_time)
+	GPIO.output(ph_down, GPIO.HIGH)
+
+
 
 if __name__ == '__main__':
 
@@ -19,6 +48,15 @@ if __name__ == '__main__':
 	GPIO.output(ph_down, GPIO.HIGH) #set relay off
 
 	print("\nSetup of Relay Module is [success]")
+
+
+
+
+
+
+
+
+	## Test
 
 	GPIO.output(ph_up, GPIO.LOW) #set relay off	GPIO.output(Relay_Ch1, GPIO.HIGH) #set relay off
 	time.sleep(15)
