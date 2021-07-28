ca1f4bf1fb9ae0f83cfe844a76f65c84df9adf25
moha7108 <moha7108@pm.me>
2021-07-28 09:03:35 -0400

    config file

--- ph-control.py
+++ ph-control.py
@@ -32,7 +32,6 @@ ph_down_status = False
 ph_monitor_status = False
 status_json = './status.json'
 
-
 ADS1115_REG_CONFIG_PGA_6_144V        = 0x00 # 6.144V range = Gain 2/3
 ADS1115_REG_CONFIG_PGA_4_096V        = 0x02 # 4.096V range = Gain 1
 ADS1115_REG_CONFIG_PGA_2_048V        = 0x04 # 2.048V range = Gain 2 (default)
@@ -54,42 +53,43 @@ def PH_up():
 	success = None
 	count = 0
 
-	### GPIO Setup
-	while success==None:
-		try:
-			GPIO.setup(ph_up,GPIO.OUT)
-			GPIO.output(ph_up, GPIO.HIGH)
-			print('\n[PH+]: Initialized PH up doser')
-			success = 1
-		except:
-			print('\n[PH+]: ERROR Initializing PH up doser')
-			pass
-
-	### Process
-	while True:
-		while PH:
+	while ph_up_status:
+		### GPIO Setup
+		while success==None:
 			try:
-				if PH < low_ph_thresh:
-					print(f'[PH+]: {PH} lower than threashold, activating pump')
-					GPIO.output(ph_up, GPIO.LOW)
-					time.sleep(dose_on_time)
-					GPIO.output(ph_up, GPIO.HIGH)
-					print(f'[PH+]: pump deactivated, waiting {dose_delay_time} seconds')
-					time.sleep(dose_delay_time)
-
-					count = 0
-				else:
-					pass
+				GPIO.setup(ph_up,GPIO.OUT)
+				GPIO.output(ph_up, GPIO.HIGH)
+				print('\n[PH+]: Initialized PH up doser')
+				success = 1
 			except:
-				count += 1
-				tries_left = retry_count-count
-				print(f'[PH+]: ERROR in PH Up control, will try {tries_left} more times')
+				print('\n[PH+]: ERROR Initializing PH up doser')
+				pass
 
-				if count >= retry_count:
-					print("[PH+]: Exceeded the number of retries, closing process... attempting to restart process")
-					thread.exit()
-				else:
-					pass
+		### Process
+		while True:
+			while PH:
+				try:
+					if PH < low_ph_thresh:
+						print(f'[PH+]: {PH} lower than threashold, activating pump')
+						GPIO.output(ph_up, GPIO.LOW)
+						time.sleep(dose_on_time)
+						GPIO.output(ph_up, GPIO.HIGH)
+						print(f'[PH+]: pump deactivated, waiting {dose_delay_time} seconds')
+						time.sleep(dose_delay_time)
+
+						count = 0
+					else:
+						pass
+				except:
+					count += 1
+					tries_left = retry_count-count
+					print(f'[PH+]: ERROR in PH Up control, will try {tries_left} more times')
+
+					if count >= retry_count:
+						print("[PH+]: Exceeded the number of retries, closing process... attempting to restart process")
+						thread.exit()
+					else:
+						pass
 
 def PH_down():
 	'''
@@ -103,42 +103,43 @@ def PH_down():
 	success = None
 	count = 0
 
-	### GPIO Setup
-	while success==None:
-		try:
-			GPIO.setup(ph_down,GPIO.OUT)
-			GPIO.output(ph_down, GPIO.HIGH)
-			print('\n[PH-]: Initialized PH down doser')
-			success = 1
-		except:
-			print('\n[PH-]: ERROR Initializing PH down doser')
-			pass
-
-	### Process
-	while True:
-		while PH:
+	while ph_down_status:
+		### GPIO Setup
+		while success==None:
 			try:
-				if PH > high_ph_thresh:
-					print(f'[PH-]: {PH} higher than the upper threashold, activating pump')
-					GPIO.output(ph_down, GPIO.LOW)
-					time.sleep(dose_on_time)
-					GPIO.output(ph_down, GPIO.HIGH)
-					print(f'[PH-]: pump deactivated, waiting {dose_delay_time} seconds')
-					time.sleep(dose_delay_time)
-
-					count = 0
-				else:
-					pass
+				GPIO.setup(ph_down,GPIO.OUT)
+				GPIO.output(ph_down, GPIO.HIGH)
+				print('\n[PH-]: Initialized PH down doser')
+				success = 1
 			except:
-				count += 1
-				tries_left = retry_count-count
-				print(f'[PH-]: ERROR in PH down control, will try {tries_left} more times')
+				print('\n[PH-]: ERROR Initializing PH down doser')
+				pass
 
-				if count >= retry_count:
-					print("[PH-]: Exceeded the number of retries, closing process... attempting to restart process")
-					thread.exit()
-				else:
-					pass
+		### Process
+		while True:
+			while PH:
+				try:
+					if PH > high_ph_thresh:
+						print(f'[PH-]: {PH} higher than the upper threashold, activating pump')
+						GPIO.output(ph_down, GPIO.LOW)
+						time.sleep(dose_on_time)
+						GPIO.output(ph_down, GPIO.HIGH)
+						print(f'[PH-]: pump deactivated, waiting {dose_delay_time} seconds')
+						time.sleep(dose_delay_time)
+
+						count = 0
+					else:
+						pass
+				except:
+					count += 1
+					tries_left = retry_count-count
+					print(f'[PH-]: ERROR in PH down control, will try {tries_left} more times')
+
+					if count >= retry_count:
+						print("[PH-]: Exceeded the number of retries, closing process... attempting to restart process")
+						thread.exit()
+					else:
+						pass
 
 
 def get_PH():
@@ -152,47 +153,47 @@ def get_PH():
 	success = None
 	count = 0
 
-	### Sensor Setup
-	while success == None:
-		try:
-			ads1115 = ADS1115() #instantiate as1115 ADC I2X Unit
-			ph = DFRobot_PH() # instantiate PH Probe
+	while ph_monitor_status:
+		### Sensor Setup
+		while success == None:
+			try:
+				ads1115 = ADS1115() #instantiate as1115 ADC I2X Unit
+				ph = DFRobot_PH() # instantiate PH Probe
 
-			ads1115.setAddr_ADS1115(AS1115_I2C_ADR) # set the I2C Address to 0x48
-			ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
+				ads1115.setAddr_ADS1115(AS1115_I2C_ADR) # set the I2C Address to 0x48
+				ads1115.setGain(ADS1115_REG_CONFIG_PGA_6_144V)
 
-			ph.reset()
-			ph.begin()
+				ph.reset()
+				ph.begin()
 
-			print("\n[PH monitor]: PH Sensor Set up Successful")
-			success = 1
+				print("\n[PH monitor]: PH Sensor Set up Successful")
+				success = 1
 
-		except:
-			print("[PH monitor]: Error Initializing PH Probe")
-			pass
-
-	### Process
-	while True:
-		try:
-			#Get the Digital Value of Analog of selected channel
-			ph_voltage = ads1115.readVoltage(ph_probe_ADC)
-			#Convert voltage to PH with temperature compensation
-			print('[PH monitor]: PH Voltage: {}, Temperature: {} ----> '.format(ph_voltage['r'],temperature), end = '')
-			PH = ph.readPH(ph_voltage['r'],temperature)
-			print("PH:{}".format(PH))
-			time.sleep(1.0)
-		except:
-			PH = None
-			count += 1
-			tries_left = retry_count-count
-			print(f'[PH monitor]: ERROR trying to Get PH data from the sensor, will try {tries_left} more times')
-
-			if count >= retry_count:
-				print("[PH monitor]: Exceeded the number of retries, closing process... attempting to restart process")
-				thread.exit()
-			else:
+			except:
+				print("[PH monitor]: Error Initializing PH Probe")
 				pass
 
+		### Process
+		while True:
+			try:
+				#Get the Digital Value of Analog of selected channel
+				ph_voltage = ads1115.readVoltage(ph_probe_ADC)
+				#Convert voltage to PH with temperature compensation
+				print('[PH monitor]: PH Voltage: {}, Temperature: {} ----> '.format(ph_voltage['r'],temperature), end = '')
+				PH = ph.readPH(ph_voltage['r'],temperature)
+				print("PH:{}".format(PH))
+				time.sleep(1.0)
+			except:
+				PH = None
+				count += 1
+				tries_left = retry_count-count
+				print(f'[PH monitor]: ERROR trying to Get PH data from the sensor, will try {tries_left} more times')
+
+				if count >= retry_count:
+					print("[PH monitor]: Exceeded the number of retries, closing process... attempting to restart process")
+					thread.exit()
+				else:
+					pass
 
 
 if __name__ == '__main__':
@@ -207,7 +208,7 @@ if __name__ == '__main__':
 		print('\nCould not initialize gpio pins')
 		exit()
 
-###### Import config file
+###### Import config file & start processes
 	try:
 		if os.path.isfile(status_json):
 			with open(status_json, "r") as f:
@@ -218,20 +219,38 @@ if __name__ == '__main__':
 			with open(status_json, "w") as f:
 				f.write(json.dumps(status, indent=4) )
 			print(f'{status_json} does not exit, new file created and formated')
+
+
+		ph_up_status = status['ph_up']
+		ph_down_status = status['ph_down']
+		ph_monitor_status = status['ph_monitor']
+
+		ph_monitor = threading.Thread(target=get_PH)
+		ph_up_control = threading.Thread(target = PH_up)
+		ph_down_control = threading.Thread(target = PH_down)
+
+		ph_monitor.start()
+		ph_up_control.start()
+		ph_down_control.start()
+
 	except:
 		print('\nCould not open or create json file of the processes')
+		exit()
 
 ##### Main Code
-	ph_monitor = threading.Thread(target=get_PH)
-	ph_up_control = threading.Thread(target = PH_up)
-	ph_down_control = threading.Thread(target = PH_down)
-
-	ph_monitor.start()
-	ph_up_control.start()
-	ph_down_control.start()
-
 
 	while True:
-		read config file
 
-		if
+		with open(status_json, "r") as f:
+			status = json.load(f)
+
+		ph_up_status = status['ph_up']
+		ph_down_status = status['ph_down']
+		ph_monitor_status = status['ph_monitor']
+
+		if ph_monitor.is_alive() == False:
+			ph_monitor.start()
+		if ph_up_control.is_alive() == False:
+			ph_up_control.start()
+		if ph_down_control.is_alive() ==False:
+			ph_down_control.start()
