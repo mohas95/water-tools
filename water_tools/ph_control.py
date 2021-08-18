7530f46371a259fac60d4e0fa0cc4e12206ad0f7
moha7108 <moha7108@pm.me>
2021-08-18 13:28:28 -0400

    try this

--- water_tools/ph_control.py
+++ water_tools/ph_control.py
@@ -118,7 +118,7 @@ class TempMonitor():
 			self.temperature = self.get_temp(self.one_wire_device_folder)
 			data = {"temperature(Celsius)":self.temperature}
 			push_to_api(self.api_file, data)
-			self.logger.info(f"[Temperatur(Celsuis)]:{self.temperature}")
+			self.logger.info(f"[Temperatur(Celsuis)]: {self.temperature}")
 			time.sleep(self.refresh_rate)
 
 		self.temperature = None
